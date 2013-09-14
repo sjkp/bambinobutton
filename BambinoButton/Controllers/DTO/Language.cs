e29403df5bc7cde66668d4eb462e110dd166ce4d
Simon Pedersen <mail@sjkp.dk>
2013-09-14 21:18:00 +0200

    Language DTO

---
+++ BambinoButton/Controllers/DTO/Language.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BalkanButton.Controllers.DTO
+{
+    public class Language
+    {
+        public string Name { get; set; }
+        public string LCID { get; set; }
+    }
+}
--- BambinoButton/Controllers/Language.cs
+++ BambinoButton/Controllers/Language.cs
@@ -6,6 +6,8 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using BalkanButton.Controllers;
+using BalkanButton.Controllers.DTO;
 
 namespace BalkanButton
 {
@@ -14,12 +16,12 @@ namespace BalkanButton
     public class LanguageController : ApiController
     {
         // GET api/<controller>
-        public IEnumerable<object> Get()
+        public List<Language> Get()
         {
             using (var context = new BalkanButton.Models.bambino_dbEntities())
             {
                 return context.song.Select(s => s.language).Distinct().ToList().Select(l => 
-                    new { Name = new CultureInfo(l).DisplayName, LCID = l }
+                    new Language { Name = new CultureInfo(l).DisplayName, LCID = l }
                     ).ToList();
             }
         }       
